78204298203bcf38ce8e0799486b954e04d2c176
YUSEF KALAM <yusefkalam@gmail.com>
2014-07-28 09:45:03 -0700

    second app.py file

--- documents/RealPython/flask-hello-world/app.py
+++ documents/RealPython/flask-hello-world/app.py
@@ -8,5 +8,31 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World!"
 
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def type1(value):
+    print(value + 1)
+    return "correct"
+
+@app.route("/float/<float:value>")
+def type2(value):
+    print(value + 1)
+    return "correct"
+
+@app.route("/path/<path:value>")
+def type3(value):
+    print(value)
+    return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "michael":
+        return "Hello, {}".format(name)
+    else:
+        return "Not Found", 404 
+
 if __name__ == "__main__":
     app.run(debug=True) 
